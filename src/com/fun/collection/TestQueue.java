88ae2a8dfcb0b3d5fe773b9b260ce1edb80b23fc
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-08 23:31:09 +0000

    Add queue example

---
+++ src/com/fun/collection/TestQueue.java
@@ -0,0 +1,38 @@
+package com.fun.collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TestQueue {
+
+	private Queue<Integer> queue;
+
+	public void testLinkedListQueue(){
+		queue = new LinkedList<Integer>();
+		for(int i = 0; i < 10; i++){
+			queue.add(i);
+		}
+		printQueue();
+		System.out.println("Removed : " + queue.remove());
+		printQueue();
+		System.out.println("Removed : " + queue.remove());
+		System.out.println("Removed : " + queue.remove());
+		printQueue();
+		queue.add(99);
+		System.out.println("Added: " + 99);
+		printQueue();
+	}
+	
+	private void printQueue(){
+		System.out.print("Queue Status: ");
+		for(Integer i : queue){
+			System.out.print(" " + i);
+		}
+		System.out.println();
+	}
+
+	public static void main(String [] args){
+		TestQueue testQueue = new TestQueue();
+		testQueue.testLinkedListQueue();
+	}
+}
