9e850b85d0533032d62c064acd7d1c5840f03421
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-13 18:08:42 +0000

    Update method reference example

--- src/com/fun/java8/MethodRef.java
+++ src/com/fun/java8/MethodRef.java
@@ -2,11 +2,21 @@ package com.fun.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class MethodRef {
 
+	static void calc(List<Integer> list, Function<Integer, Double> fun){
+		for(int n : list){
+			System.out.println(n + " -> " + fun.apply(n));
+		}
+	}
+
 	public static void main(String[] args) {
 		List<Integer>  numbers = Arrays.asList(4,9,16,25,36);
-		numbers.forEach(System.out::println);
+		System.out.println("Sqaures of numbers: ");
+		MethodRef.calc(numbers, Math::sqrt);
+		System.out.println("\nSin(x) of numbers: ");
+		MethodRef.calc(numbers, Math::sin);
 	}
 }
