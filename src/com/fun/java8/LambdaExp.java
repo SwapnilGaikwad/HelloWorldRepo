10d02a0b9f032b47012ea4050f28007325c12dd4
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-15 21:47:42 +0000

    Update java lambda example

--- src/com/fun/java8/LambdaExp.java
+++ src/com/fun/java8/LambdaExp.java
@@ -2,12 +2,29 @@ package com.fun.java8;
 
 public class LambdaExp {
 
+	private interface SayMessage {
+		void sayHello(String message);
+	}
+
+	public void testLambda(){
+		Runnable r1 = new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Executing for anonymous implementation of runnable r1");
+			}
+		};
+		
+		Runnable r2 = () -> System.out.println("Executing for lambda implementation of runnable r2");
+		r1.run();
+		r2.run();
+	}
+
 	public static void main(String[] args) {
 		SayMessage msg = (String message) -> System.out.println("Hello " + message);
 		msg.sayHello("TestName");
-	}
-
-	private interface SayMessage {
-		void sayHello(String message);
+		
+		LambdaExp obj = new LambdaExp();
+		obj.testLambda();
 	}
 }
