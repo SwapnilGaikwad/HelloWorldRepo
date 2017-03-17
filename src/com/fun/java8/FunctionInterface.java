41d792670bad7412cb9aa84bdf4ba362e3204ff3
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-17 01:04:38 +0000

    Improve function interface example

--- src/com/fun/java8/FunctionInterface.java
+++ src/com/fun/java8/FunctionInterface.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 
 public class FunctionInterface {
 
-	List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6);
 
 	private void evaluate(List<Integer> listOfInts, Predicate<Integer> predicate){
 		for(Integer n : listOfInts){
@@ -19,6 +19,12 @@ public class FunctionInterface {
 	public void test(){
 		System.out.println("Running with always true predicate...");
 		evaluate(ints, val->true);
+		
+		System.out.println("Running with always false predicate...");
+		evaluate(ints, test->false);
+		
+		System.out.println("Printing only even numbers...");
+		evaluate(ints, val->val % 2 == 0);
 	}
 	public static void main(String[] args) {
 		new FunctionInterface().test();
