492ddf7b05cfebdad3cf53a284a887d69d160287
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-18 16:16:11 +0000

    Improve forEach example

--- src/com/fun/java8/ForEach.java
+++ src/com/fun/java8/ForEach.java
@@ -5,15 +5,24 @@ import java.util.List;
 
 public class ForEach {
 
-	public static void main(String[] args) {
-
-		List<Integer> list = Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		
-		System.out.print("List contents: ");
+	public void simpleForEach(List<Integer> list){
+		System.out.print("\nList contents: ");
 		for (Integer integer : list) {
 			System.out.print(integer + " ");
 		}
-		System.out.println();
+	}
+
+	public void java8ForEach(List<Integer> list){
+		System.out.print("\nList contents: ");
+		list.forEach((val) -> System.out.print(val + " "));
+	}
+
+	public static void main(String[] args) {
+
+		List<Integer> list = Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		ForEach obj = new ForEach();
+		obj.simpleForEach(list);
+		obj.java8ForEach(list);
 	}
 
 }
