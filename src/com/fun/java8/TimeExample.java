65d3126acc7c2e38f2c7b1ecc8d7fde7d865f278
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-29 01:42:54 +0100

    Improve Java 8 date time example

--- src/com/fun/java8/TimeExample.java
+++ src/com/fun/java8/TimeExample.java
@@ -1,6 +1,8 @@
 package com.fun.java8;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class TimeExample {
@@ -9,8 +11,11 @@ public class TimeExample {
 
 		Date oldJavaDate = new Date();
 		LocalDate newJavaDate = LocalDate.now();
+		LocalDateTime newJavaDateTime = LocalDateTime.now();
 		
 		System.out.println("Old Java date : " + oldJavaDate);
 		System.out.println("New Java date : " + newJavaDate);
+		System.out.println("Local Date time: " + newJavaDateTime);
+		System.out.println("Local time in India: " + LocalDateTime.now(ZoneId.of("Asia/Kolkata")));
 	}
 }
