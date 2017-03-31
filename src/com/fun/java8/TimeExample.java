df39e2f00a6b49f3e09fa607864b4598c49689a9
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-31 13:49:07 +0100

    Improve java time example

--- src/com/fun/java8/TimeExample.java
+++ src/com/fun/java8/TimeExample.java
@@ -3,9 +3,21 @@ package com.fun.java8;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class TimeExample {
+
+	public void testChronoUnits(){
+		System.out.println("********************************************");
+		LocalDate today = LocalDate.now();
+		System.out.println("Today: " + today);
+		System.out.println("Tomorrow: " + today.plus(1, ChronoUnit.DAYS));
+		System.out.println("Next week: " + today.plus(1, ChronoUnit.WEEKS));
+		System.out.println("Next month: " + today.plus(1, ChronoUnit.MONTHS));
+		System.out.println("Next year: " + today.plus(1, ChronoUnit.YEARS));
+		System.out.println("Next decade: " + today.plus(1, ChronoUnit.DECADES));
+	}
 
 	public static void main(String[] args) {
 
@@ -17,5 +29,7 @@ public class TimeExample {
 		System.out.println("New Java date : " + newJavaDate);
 		System.out.println("Local Date time: " + newJavaDateTime);
 		System.out.println("Local time in India: " + LocalDateTime.now(ZoneId.of("Asia/Kolkata")));
+		
+		new TimeExample().testChronoUnits();
 	}
 }
