1f46c91a363dc47c6689bd86192ee04cf564b9c8
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-04-01 09:37:43 +0100

    Update time example

--- src/com/fun/java8/TimeExample.java
+++ src/com/fun/java8/TimeExample.java
@@ -2,10 +2,15 @@ package com.fun.java8;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+/**
+ * @author sgaikwad
+ *
+ */
 public class TimeExample {
 
 	public void testChronoUnits(){
@@ -19,6 +24,13 @@ public class TimeExample {
 		System.out.println("Next decade: " + today.plus(1, ChronoUnit.DECADES));
 	}
 
+	public void testPeriod(){
+		LocalDate currentDate = LocalDate.now();
+		LocalDate futureDate = currentDate.plus(1, ChronoUnit.MONTHS);
+		System.out.println("********************************************");
+		System.out.println("Duration between " + currentDate + " and future date: "+ futureDate + " is : " + Period.between(currentDate, futureDate).getMonths());
+	}
+
 	public static void main(String[] args) {
 
 		Date oldJavaDate = new Date();
@@ -30,6 +42,9 @@ public class TimeExample {
 		System.out.println("Local Date time: " + newJavaDateTime);
 		System.out.println("Local time in India: " + LocalDateTime.now(ZoneId.of("Asia/Kolkata")));
 		
-		new TimeExample().testChronoUnits();
+		TimeExample obj = new TimeExample();
+		obj.testChronoUnits();
+		obj.testPeriod();
+		
 	}
 }
