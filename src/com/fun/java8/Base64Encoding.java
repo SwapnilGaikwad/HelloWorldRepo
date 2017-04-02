5e7fbff167e88848b6332efd0632f54130f00c6b
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-04-02 01:29:22 +0100

    Add base64 encoding example

---
+++ src/com/fun/java8/Base64Encoding.java
@@ -0,0 +1,20 @@
+package com.fun.java8;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+public class Base64Encoding {
+
+	public static void main(String[] args) {
+		try {
+			String base64Encoding = Base64.getEncoder().encodeToString("Encoded string".getBytes("utf-8"));
+			System.out.println("Encoding string: " + base64Encoding);
+			byte [] origStr = Base64.getDecoder().decode(base64Encoding);
+			System.out.println("Decoded string: " + new String(origStr, "utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
