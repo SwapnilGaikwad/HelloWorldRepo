13b103de859a0429a5a3cc353ccca9cfeaf264cb
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-04-08 23:05:39 +0100

    Add annotation example

--- src/com/fun/misc/AnnotationProcesser.java
+++ src/com/fun/misc/AnnotationProcesser.java
@@ -1,9 +1,39 @@
 package com.fun.misc;
 
-public class AnnotationProcesser {
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 
-	public static void main(String[] args) {
 
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface TestAnnotation {
+	String name() default "defaultName";
+	int revision() default 1;
+}
+
+class SimpleAnnotation {
+
+	@Override
+	@TestAnnotation(name="testName", revision=5)
+	public String toString(){
+		return  "SimpleAnnotation to string method";
 	}
+}
 
+public class AnnotationProcesser {
+	
+	public static void main(String[] args) throws SecurityException, ClassNotFoundException{
+		ClassLoader classLoader = AnnotationProcesser.class.getClassLoader();
+		for(Method method: classLoader.loadClass("com.fun.misc.SimpleAnnotation").getMethods()){
+			for(Annotation annotation : method.getDeclaredAnnotations()){
+				System.out.println("Annotation: " + annotation);
+				TestAnnotation testAnnotation = method.getAnnotation(TestAnnotation.class);
+				System.out.println("Revision: " + testAnnotation.revision());
+				System.out.println("Name: " + testAnnotation.name());}
+		}
+	}
 }
